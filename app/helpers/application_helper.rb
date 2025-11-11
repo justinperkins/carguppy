6ea383d85e2b8a6001152fc342ceb5fa0603a108
Justin Perkins <tacovoyager@gmail.com>
2025-11-10 23:05:44 -0500

    rubocop corrections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def nav_item_classes
-    'relative top-[2px]'
+    "relative top-[2px]"
   end
 
   def nav_item_link_classes(nav_item_path)
     classes = %w[p-2 no-underline]
-    classes << 'border-b-4 border-emerald-600' if current_page?(nav_item_path)
-    classes.join(' ')
+    classes << "border-b-4 border-emerald-600" if current_page?(nav_item_path)
+    classes.join(" ")
   end
 end
